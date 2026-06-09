0b8a8c0ef2058d13f7ce0c7140b3bee1e6d2cf99
unknown <hernanel>
2026-06-09 08:55:31 -0400

    feat: configuracion de URL de produccion para el backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Creamos la conexión base apuntando a tu backend de NestJS
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // El puerto donde corre tu backend
+  baseURL: 'https://tienda-backend-q62q.onrender.com', // El puerto donde corre tu backend
   headers: {
     'Content-Type': 'application/json',
   },
